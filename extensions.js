2f5f87bf5c7b4ba8f1e0e7deb766fd1e6136bc47
Ícaro Harry <me@icaro.io>
2018-01-15 18:38:09 -0200

    Fixes for electron build

--- extensions.js
+++ extensions.js
@@ -4,41 +4,37 @@ const { ipcMain, BrowserWindow } = require('electron');
 
 const METAMASK_ID = 'nkbihfbeogaeaoehlefnkodbefgpgknn';
 
-const loadMetamask = (session, window) => {
+const getMetamaskPath = (isDev) => {
+  if (isDev) {
+    return path.join(__dirname,`extensions/metamask/`);
+  }
+  return path.resolve(__dirname,`../../app.asar.unpacked/dist/extensions/metamask/`);
+};
+
+const loadMetamask = (session, window, isDev) => {
   let manifest;
   let metamaskPopup;
   let metamaskNotification;
   let mainWindow = window;
+  const metamaskPath = getMetamaskPath(isDev);
 
-  fs.readFile(path.join(__dirname,`extensions/metamask/manifest.json`), 'utf8', function (err, data) {
+  fs.readFile(`${metamaskPath}/manifest.json`, 'utf8', function (err, data) {
     if (err) throw err;
     manifest = JSON.parse(data);
-    manifest.content_scripts[0].include_globs = ['chrome://brave*'];
-    // manifest.permissions.push('<all_urls>');
-    // manifest.permissions.push('tabs');
-    console.log(manifest)
-    session.defaultSession.extensions.load(path.join(__dirname,`extensions/metamask`), manifest, 'component');
+    session.defaultSession.extensions.load(metamaskPath, manifest, 'component');
   });
 
   ipcMain.on('open-metamask-popup', (event, arg) => {
-    console.log(require('electron').screen.getAllDisplays())
     if(metamaskPopup && !metamaskPopup.isDestroyed()) metamaskPopup.close();
 
     metamaskPopup = new BrowserWindow({
       title: 'MetaMask',
       width: 360,
       height: 520,
-      type: 'popup'
-      //resizable: false,
+      type: 'popup',
+      resizable: false
     });
     metamaskPopup.loadURL(`chrome-extension://${METAMASK_ID}/popup.html`);
-    // fn = '(function(w) {console.log(w);w.type = "popup"})(window)'
-    // metamaskPopup.webContents.openDevTools();
-    // setTimeout(() => {
-    //   metamaskPopup.webContents.executeScriptInTab(METAMASK_ID, fn, {}, (error, on_url, results) => {
-    //     console.log(error, on_url, results)
-    //   });
-    // }, 100);
   });
 
   ipcMain.on('open-metamask-notification', (event, arg) => {
@@ -48,8 +44,8 @@ const loadMetamask = (session, window) => {
       title: 'MetaMask',
       width: 360,
       height: 520,
-      type: 'popup'
-      //resizable: false,
+      type: 'popup',
+      resizable: false
     });
     metamaskNotification.loadURL(`chrome-extension://${METAMASK_ID}/notification.html`);
   });
--- main.js
+++ main.js
@@ -1,41 +1,43 @@
 const { app, BrowserWindow, shell, session } = require('electron');
 const path = require('path');
 const url = require('url');
-// const isDev = require('electron-is-dev');
 const fs = require('fs');
 
 const extensions = require('./extensions');
 
-// if(isDev) {
-//   require('electron-reload')(path.join(__dirname));
-// }
+let isDev;
+try {
+  isDev = require('electron-is-dev');
+} catch(e) {
+  isDev = false;
+}
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
+if(isDev) {
+  require('electron-reload')(path.join(__dirname));
+}
+
 let win;
 
 function createWindow() {
-  // Create the browser window.
   win = new BrowserWindow({
     width: 1200,
-    height: 800,
-    icon: 'assets/swapy3.png',
+    height: 800
   });
 
-  extensions.loadMetamask(session, win);
+  extensions.loadMetamask(session, win, isDev);
 
-  // win.webContents.executeJavaScript("window.isElectron=true;")
+  let indexPath;
+  isDev ? indexPath = path.join(`brave/${__dirname}`, 'app/dist/index.html') : indexPath = path.join(`brave/${__dirname}`, 'index.html');
 
   win.loadURL(url.format({
-    pathname: path.join(`brave/${__dirname}`, 'index.html'),
+    pathname: indexPath,
     protocol: 'chrome',
     slashes: true
   }));
 
-  //if(isDev) {
-    // Open the DevTools.
+  if(isDev) {
     win.webContents.openDevTools();
-  //}
+  }
 
   win.on('closed', () => {
     win = null;
@@ -45,16 +47,12 @@ function createWindow() {
 app.on('ready', createWindow);
 
 app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
     app.quit();
   }
 });
 
 app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (win === null) {
     createWindow();
   }
