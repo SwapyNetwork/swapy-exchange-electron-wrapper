54513a35678bd188a12e001a1ae7d4ac0e23e615
Ícaro Harry <me@icaro.io>
2018-01-17 12:38:28 -0200

    Fix loading metamask issue

--- extensions.js
+++ extensions.js
@@ -3,6 +3,7 @@ const path = require('path');
 const { ipcMain, BrowserWindow } = require('electron');
 
 const METAMASK_ID = 'nkbihfbeogaeaoehlefnkodbefgpgknn';
+const METAMASK_KEY = 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAlcgI4VVL4JUvo6hlSgeCZp9mGltZrzFvc2Asqzb1dDGO9baoYOe+QRoh27/YyVXugxni480Q/R147INhBOyQZVMhZOD5pFMVutia9MHMaZhgRXzrK3BHtNSkKLL1c5mhutQNwiLqLtFkMSGvka91LoMEC8WTI0wi4tACnJ5FyFZQYzvtqy5sXo3VS3gzfOBluLKi7BxYcaUJjNrhOIxl1xL2qgK5lDrDOLKcbaurDiwqofVtAFOL5sM3uJ6D8nOO9tG+T7hoobRFN+nxk43PHgCv4poicOv+NMZQEk3da1m/xfuzXV88NcE/YRbRLwAS82m3gsJZKc6mLqm4wZHzBwIDAQAB';
 
 const getMetamaskPath = (isDev) => {
   if (isDev) {
@@ -15,6 +16,9 @@ const loadMetamaskFromManifest = (session, metamaskPath) => {
   fs.readFile(`${metamaskPath}/manifest.json`, 'utf8', function (err, data) {
     if (err) throw err;
     manifest = JSON.parse(data);
+    manifest.content_scripts[0].matches = ['chrome://brave/**/*'];
+    manifest.key = METAMASK_KEY;
+    console.log(manifest)
     session.defaultSession.extensions.load(metamaskPath, manifest, 'component');
   });
 };
